ce45e1df8aa0590db17ef5b4b221273541ca87d6
Erik Madsen <emadsen@hbmachining.com>
2022-10-20 00:09:39 -0700

    updates price ticker and adds new onion address for electrum server

--- wallet/src/main/java/com/verge/wallet/Constants.java
+++ wallet/src/main/java/com/verge/wallet/Constants.java
@@ -110,8 +110,8 @@ public class Constants {
                                                     new ServerAddress("ltc-testnet-cce-2.verge.net", 15002)),
 			new CoinAddress(NxtMain.get(),          new ServerAddress("176.9.65.41", 7876),
                                                     new ServerAddress("176.9.65.41", 7876)),
-            new CoinAddress(VergeMain.get(),        new ServerAddress("qz6pnufyqjexudq6.onion", 50001, TOR_LOCAL_PROXY),
-                                                    new ServerAddress("wf5x2ezz3zkoxhbz7durh46xgpr2m7f6p46hqhxpzyyy7xx7gjvrltyd.onion", 50001, TOR_LOCAL_PROXY)) //add v3 electrum
+            new CoinAddress(VergeMain.get(),        new ServerAddress("cc64ehadi6tho6ncinprwdl3tubzw34moo2lqtoiepcw6653lsqp3kad.onion", 50001, TOR_LOCAL_PROXY),
+                                                    new ServerAddress("cc64ehadi6tho6ncinprwdl3tubzw34moo2lqtoiepcw6653lsqp3kad.onion", 50001, TOR_LOCAL_PROXY)) //add v3 electrum
     );
 
     public static final HashMap<CoinType, Integer> COINS_ICONS;
